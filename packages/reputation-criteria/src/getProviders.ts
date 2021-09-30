f727769d7efefd9c8e2699169b9cdb9d98413826
cedoor <me@cedoor.dev>
2021-09-30 11:42:22 +0200

    fix: remove spread array

--- packages/reputation-criteria/src/getProviders.ts
+++ packages/reputation-criteria/src/getProviders.ts
@@ -5,5 +5,8 @@ import { Provider, Web2Provider, Web3Provider } from "./types/criteria"
  * @returns A list of existing providers.
  */
 export default function getProviders(): Provider[] {
-    return [...Object.values(Web2Provider), ...Object.values(Web3Provider)]
+    const web3Providers: Provider[] = Object.values(Web3Provider)
+    const web2Providers: Provider[] = Object.values(Web2Provider)
+
+    return web2Providers.concat(web3Providers)
 }
