1789f3ab0350dfe32e1d889fbbfb06cdfb23d772
cedoor <me@cedoor.dev>
2021-10-22 17:38:02 +0200

    feat: add sha256 as export function

--- packages/telegram-bot/src/index.ts
+++ packages/telegram-bot/src/index.ts
@@ -1,5 +1,6 @@
 import InterRepBot from "./bot"
 import TelegramUser from "./model/TelegramUser.model"
+import sha256 from "./sha256"
 
-export { InterRepBot, TelegramUser }
+export { InterRepBot, TelegramUser, sha256 }
 export * from "./model/TelegramUser.types"
