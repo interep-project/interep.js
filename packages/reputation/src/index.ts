860292f5dd794a4aeeb76c4e582e8f2db666cfdd
cedoor <me@cedoor.dev>
2022-03-25 20:26:52 +0100

    fix: add missing export

--- packages/reputation/src/index.ts
+++ packages/reputation/src/index.ts
@@ -1,10 +1,19 @@
 import calculateReputation from "./calculateReputation"
 import getReputationLevels from "./getReputationLevels"
+import getReputationCriteria from "./getReputationCriteria"
 import getOAuthProviders from "./getOAuthProviders"
 import twitterCriteria from "./criteria/twitter"
 import githubCriteria from "./criteria/github"
 import redditCriteria from "./criteria/reddit"
 
-export { calculateReputation, getReputationLevels, getOAuthProviders, twitterCriteria, githubCriteria, redditCriteria }
+export {
+    calculateReputation,
+    getReputationLevels,
+    getReputationCriteria,
+    getOAuthProviders,
+    twitterCriteria,
+    githubCriteria,
+    redditCriteria
+}
 export * from "./types/criteria"
 export * from "./types/providerParameters"
