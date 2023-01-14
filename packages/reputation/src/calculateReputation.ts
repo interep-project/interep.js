7073806ac34fc99e316fb194e4edc117a9ecfcac
Dimitris Apostolou <dimitris.apostolou@icloud.com>
2023-01-14 19:04:10 +0200

    fix: fix typos

--- packages/reputation/src/calculateReputation.ts
+++ packages/reputation/src/calculateReputation.ts
@@ -5,26 +5,26 @@ import { ProviderParameters } from "./types/providerParameters"
 /**
  * Returns the reputation based on the parameters.
  * @param provider The provider.
- * @param paramaters The provider parameters to check.
+ * @param parameters The provider parameters to check.
  * @returns The reputation level found.
  */
-export default function calculateReputation(provider: OAuthProvider, paramaters: ProviderParameters): ReputationLevel {
+export default function calculateReputation(provider: OAuthProvider, parameters: ProviderParameters): ReputationLevel {
     const criteria = getReputationCriteria(provider)
     const providerParameterNames = criteria.parameters.map((parameter: any) => parameter.name)
     const providerParameterTypes = criteria.parameters.map((parameter: any) => parameter.type)
 
-    for (const parameterName in paramaters) {
-        if (Object.prototype.hasOwnProperty.call(paramaters, parameterName)) {
+    for (const parameterName in parameters) {
+        if (Object.prototype.hasOwnProperty.call(parameters, parameterName)) {
             const parameterIndex = providerParameterNames.indexOf(parameterName)
 
             if (parameterIndex === -1) {
                 throw new Error(`Parameter '${parameterName}' is not supported`)
             }
 
-            const paramaterValue = paramaters[parameterName as keyof ProviderParameters]
+            const parameterValue = parameters[parameterName as keyof ProviderParameters]
             const expectedType = providerParameterTypes[parameterIndex]
 
-            if (typeof paramaterValue !== expectedType) {
+            if (typeof parameterValue !== expectedType) {
                 throw new TypeError(`Parameter '${parameterName}' is not a ${expectedType}`)
             }
         }
@@ -33,7 +33,7 @@ export default function calculateReputation(provider: OAuthProvider, paramaters:
     for (const reputation of criteria.reputationLevels) {
         for (const rule of reputation.rules) {
             if (rule.value !== null) {
-                const parameterValue = paramaters[rule.parameter as keyof ProviderParameters]
+                const parameterValue = parameters[rule.parameter as keyof ProviderParameters]
 
                 if (parameterValue !== undefined) {
                     if (typeof rule.value !== "object") {
